de0e6d684f9d12a1647a30043a75974fe3fa9933
Zaur Rajabov <zradj7@gmail.com>
2026-02-17 02:07:14 +0400

    add config file support

--- src/main/abstract_factory_prototype/GUIApp.java
+++ src/main/abstract_factory_prototype/GUIApp.java
@@ -1,49 +1,67 @@
 package main.abstract_factory_prototype;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class GUIApp {
 
-    private static Application configureApplication() {
+    private static Application configureApplication() throws IOException {
         Application app;
         GUIFactory factory;
 
-        // let's pretend we have some functionality to detect the system theme
-        String theme = "light";
+        Properties prop = new Properties();
+        String propFileName = "config.properties";
 
-        String osName = System.getProperty("os.name").toLowerCase();
+        try (InputStream input = new FileInputStream(propFileName)) {
+            prop.load(input);
 
-        if (osName.contains("mac")) {
-            if (theme.equals("light")) {
-                factory = new GUIFactory.MacLightGUIFactory();
-            } else if (theme.equals("dark")) {
-                factory = new GUIFactory.MacDarkGUIFactory();
-            } else {
-                throw new RuntimeException("Unrecognized theme: " + theme);
+            String theme = prop.getProperty("os.theme");
+            String osName = prop.getProperty("os.name");
+
+            if (theme == null) {
+                throw new RuntimeException("Theme was not found, please define os.theme in config.properties");
             }
-        } else if (osName.contains("win")) {
-            if (theme.equals("light")) {
-                factory = new GUIFactory.WinLightGUIFactory();
-            } else if (theme.equals("dark")) {
-                factory = new GUIFactory.WinDarkGUIFactory();
-            } else {
-                throw new RuntimeException("Unrecognized theme: " + theme);
+
+            if (osName == null) {
+                throw new RuntimeException("OS name was not found, please define os.name in config.properties");
             }
-        } else if (osName.contains("linux")) {
-            if (theme.equals("light")) {
-                factory = new GUIFactory.LinuxLightGUIFactory();
-            } else if (theme.equals("dark")) {
-                factory = new GUIFactory.LinuxDarkGUIFactory();
+
+            if (osName.contains("mac")) {
+                if (theme.equals("light")) {
+                    factory = new GUIFactory.MacLightGUIFactory();
+                } else if (theme.equals("dark")) {
+                    factory = new GUIFactory.MacDarkGUIFactory();
+                } else {
+                    throw new RuntimeException("Unrecognized theme: " + theme);
+                }
+            } else if (osName.contains("win")) {
+                if (theme.equals("light")) {
+                    factory = new GUIFactory.WinLightGUIFactory();
+                } else if (theme.equals("dark")) {
+                    factory = new GUIFactory.WinDarkGUIFactory();
+                } else {
+                    throw new RuntimeException("Unrecognized theme: " + theme);
+                }
+            } else if (osName.contains("linux")) {
+                if (theme.equals("light")) {
+                    factory = new GUIFactory.LinuxLightGUIFactory();
+                } else if (theme.equals("dark")) {
+                    factory = new GUIFactory.LinuxDarkGUIFactory();
+                } else {
+                    throw new RuntimeException("Unrecognized theme: " + theme);
+                }
             } else {
-                throw new RuntimeException("Unrecognized theme: " + theme);
+                throw new RuntimeException("Unrecognized OS: " + osName);
             }
-        } else {
-            throw new RuntimeException("Unrecognized OS: " + osName);
+
+            app = new Application(factory);
+            return app;
         }
-
-        app = new Application(factory);
-        return app;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Application app = configureApplication();
         app.paint();
     }
