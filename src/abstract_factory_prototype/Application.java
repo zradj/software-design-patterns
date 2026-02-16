051dce6795dc9a83d2b638e6c881e9775fae8f54
Zaur Rajabov <zradj7@gmail.com>
2026-02-17 00:05:03 +0400

    add progressBar creation to application

--- src/abstract_factory_prototype/Application.java
+++ src/abstract_factory_prototype/Application.java
@@ -2,19 +2,23 @@ package abstract_factory_prototype;
 
 import abstract_factory_prototype.Element.Button;
 import abstract_factory_prototype.Element.Checkbox;
+import abstract_factory_prototype.Element.ProgressBar;
 
 public class Application {
 
     private Button button;
     private Checkbox checkbox;
+    private ProgressBar progressBar;
 
     public Application(GUIFactory factory) {
         this.button = factory.createButton();
         this.checkbox = factory.createCheckbox();
+        this.progressBar = factory.createProgressBar();
     }
 
     public void paint() {
         button.paint();
         checkbox.paint();
+        progressBar.paint();
     }
 }
