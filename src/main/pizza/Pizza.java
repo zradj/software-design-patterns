f99d322345914e1d8f0d781d4f3d003b73061b9c
DOM <zradj7@gmail.com>
2026-03-10 09:14:59 +0400

    add some bad code

---
+++ src/main/pizza/Pizza.java
@@ -0,0 +1,31 @@
+package main.pizza;
+
+public class Pizza {
+
+  private boolean cheese;
+  private boolean pepperoni;
+
+  public void eat() {
+    System.out.println("Eating pizza");
+  }
+
+  public void thank() {
+    System.out.println("Thanks");
+  }
+
+  public void addCheese() {
+      this.cheese = true;
+  }
+
+  public void addPepperoni() {
+      this.pepperoni = true;
+  }
+
+  public boolean hasCheese() {
+      return this.cheese;
+  }
+
+  public boolean hasPepperoni() {
+      return this.pepperoni;
+  }
+}
